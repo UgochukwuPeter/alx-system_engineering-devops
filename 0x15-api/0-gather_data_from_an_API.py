e8e5f4ed277bf8bcb0419ad739f1097a6ac6cfed
UgochukwuPeter <pjgraphixs@gmail.com>
2024-03-26 02:38:32 +0100

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,59 +1,37 @@
 #!/usr/bin/python3
-"""script that fetches info about a given employee's ID using an api"""
-import json
+# Write a Python script that, using this REST API, for a given
+# employee ID, returns information about his/her Todo list progress.
+
 import requests
 import sys
 
 
-base_url = 'https://jsonplaceholder.typicode.com'
-
 if __name__ == "__main__":
+    # Check if the script is provided with an employee ID as a command-line argument
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-    user_id = sys.argv[1]
+    employee_ID = sys.argv[1]
+    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
+    url = f'{jsonplaceholder}/{employee_ID}'
 
-    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
-    user_url = '{}/users?id={}'.format(base_url, user_id)
-    # print("user url is: {}".format(user_url))
+    # Make a GET request to the API
+    response = requests.get(url)
 
-    # get info from api
-    response = requests.get(user_url)
-    # pull data from api
-    data = response.text
-    # parse the data into JSON format
-    data = json.loads(data)
-    # extract user data, in this case, name of employee
-    name = data[0].get('name')
-    # print("id is: {}".format(user_id))
-    # print("name is: {}".format(name))
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        employee_name = response.json().get('name')
+        Todourl = f'{url}/todos'
+        res = requests.get(Todourl)
+        tasks = res.json()
 
-    # get user info about todo tasks
-    # e.g https://jsonplaceholder.typicode.com/users/1/todos
-    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
-    # print("tasks url is: {}".format(tasks_url))
+        # Filter completed tasks
+        done_tasks = [task for task in tasks if task.get('completed')]
 
-    # get info from api
-    response = requests.get(tasks_url)
-    # pull data from api
-    tasks = response.text
-    # parse the data into JSON format
-    tasks = json.loads(tasks)
-
-    # initialize completed count as 0 and find total number of tasks
-    completed = 0
-    total_tasks = len(tasks)
-
-    # initialize empty list for completed tasks
-    completed_tasks = []
-    # loop through tasks counting number of completed tasks
-    for task in tasks:
-
-        if task.get('completed'):
-            # print("The tasks are: {}\n".format(task))
-            completed_tasks.append(task)
-            completed += 1
-
-    # print the output in the required format
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, completed, total_tasks))
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+        # Display the employee TODO list progress
+        print("Employee {} is done with tasks({}/{}):".format(employee_name, len(done_tasks), len(tasks)))
+        for task in done_tasks:
+            print("\t{}".format(task.get('title')))
+    else:
+        # Display an error message if the request was not successful
+        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
